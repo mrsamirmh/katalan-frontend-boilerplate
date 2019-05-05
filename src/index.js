240c2249a6f6b1641175f58fedbd4c27cd7a6fd2
Samir Memmedhesenov <samirmh00@gmail.com>
2019-05-05 15:33:14 +0400

    Fixed bug on image imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+
 import './scss/main.scss'
 
 require('./js/main');
+
+const imageFiles = fs.readdirSync('./img');
+imageFiles.map(item => {
+    // Split names and extension
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+
+    require('./img/' + name + '.' + extension);
+});
